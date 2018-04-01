78b5496e5b142517807413067395cceb4f74e389
Amartya Chaudhuri <amchau@microsoft.com>
2018-04-01 09:51:50 +0530

    Add constants with env variables

--- src/constants.py
+++ src/constants.py
@@ -1,8 +1,14 @@
+import os
 
 #SMTP details for Outlook
 SMTP_SERVER_OUTLOOK = "smtp-mail.outlook.com"
 SMTP_SERVER_PORT_OUTLOOK = 587
 
 #Source mail authentication details
-SOURCE_MAIL = "team_nestorbot@hotmail.com"
-SOURCE_PASSWORD = "password"
+SOURCE_MAIL = os.environ["SOURCE_MAIL"]
+SOURCE_PASSWORD = os.environ["SOURCE_PASSWORD"]
+
+#LUIS Specifications
+LUIS_APP_KEY = os.environ['LUIS_APP_KEY']
+LUIS_API_SUBSCRIPTION = os.environ['LUIS_API_SUBSCRIPTION']
+
